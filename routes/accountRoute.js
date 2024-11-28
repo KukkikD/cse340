e8eb8cc4800084dfa4c4b912f8d328c161ff64a0
KukkikD <adeezon9@gmail.com>
2024-11-28 04:57:53 -0800

    add veridation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,7 +2,8 @@
 const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
-const utilities = require("../utilities/index.js");
+const utilities = require("../utilities/index.js")
+const regValidate = require('../utilities/account-validation')
 
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -10,8 +11,9 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-// Route to process account registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the registration data
+router.post('/register', regValidate.registationRules(),
+regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
 
 
 module.exports = router;
